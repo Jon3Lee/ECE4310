720fc5abb4b0711ca306d6a011011cd1e6352667
willt2021 <79667361+willt2021@users.noreply.github.com>
2021-03-26 11:29:04 -0700

    New changes for algorithm to find safety of k

--- BankersAlgorithm/BankersAlgorithm.cpp
+++ BankersAlgorithm/BankersAlgorithm.cpp
@@ -1,36 +1,37 @@
 // Banker's Algorithm 
 #include <iostream> 
-using namespace std;
+//using namespace std;
 
 // This is sub branch 1.
 int main()
 {
-	// P0, P1, P2, P3, P4 are the Process names here 
-
+	int input;	// The input that is put as variable k in the midterm.
 	int  i, j, k;
+	std::cout << "Please enter the k you would like to test for safety:" << std::endl;
+	std::cin >> input;
 	const int n = 3; // Number of processes 
 	const int  m = 2; // Number of resources 
 	int need[n][m];
-	int allocation[3][1] = { { 0 }, // A // Allocation Matrix 
-						{ 2 }, // B 
-						{ 3 }, // C
+	int allocation[3][1] = { { 15 }, // A // Allocation Matrix 
+						{ 8 }, // B 
+						{ 7 } }; // C
 		
 
-	int max[3][2] = { { 7 }, // A // MAX Matrix 
-					{ 3 }, // B
-					{ 9 }, // C 
+	int max[3][1] = { { 38 }, // A // MAX Matrix 
+					{ 24 }, // B
+					{ 30 } }; // C 
 					
 
-	int avail[3] = { 3, 3, 2 }; // Available Resources 
+	int avail[1] = { 3 }; // Available Resources, or in the Midterm, it is "free"
 
-	int f[n],ans[n], ind = 0;
+	int f[n],ans[n], ind = 0;		
 	for (k = 0; k < n; k++) {
 		f[k] = 0;
 	}
 
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++)
-			need[i][j] = max[i][j] - allocation[i][j];
+			need[i][j] = max[i][j] - allocation[i][j];	// Need = Max - Available
 	}
 	int y = 0;
 	for (k = 0; k < 5; k++) {
@@ -55,13 +56,18 @@ int main()
 		}
 	}
 
-	cout << "Following is the SAFE Sequence" << endl;
+	std::cout << "Following is the SAFE Sequence..." << std::endl;
 	for (i = 0; i < n - 1; i++)
-		cout << " P" << ans[i] << " ->";
-	cout << " P" << ans[n - 1] << endl;
-
+		//if (-500 <= input && input <= 500) {	// This does not work, please REVISE.
+		//	std::cout << "The input: " << input << " is not safe!" << std::endl;
+		//}
+		//else {
+			std::cout << " P" << ans[i] << " ->";
+			std::cout << " P" << ans[n - 1] << std::endl;
+		//}
 	return (0);
 }
 
 // This code is contributed by SHUBHAMSINGH10 
+// ADDITIONAL INFO: This code was modified by willt2021
 
